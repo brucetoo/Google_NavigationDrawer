0a268056fbeb61b24aba0c039ebf059ef77e75d2
brucetoo <brucetoo14@gmail.com>
2015-01-23 17:52:43 +0800

    加入gif动画的处理框架

--- app/src/main/java/com/example/bruce/navigationdrawer/PlaceholderFragment.java
+++ app/src/main/java/com/example/bruce/navigationdrawer/PlaceholderFragment.java
@@ -7,14 +7,23 @@ package com.example.bruce.navigationdrawer;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import pl.droidsonroids.gif.GifDrawable;
+import pl.droidsonroids.gif.GifImageView;
+
 /**
  * A placeholder fragment containing a simple view.
  */
-public  class PlaceholderFragment extends Fragment {
+public class PlaceholderFragment extends Fragment {
     /**
      * The fragment argument representing the section number for this
      * fragment.
@@ -41,8 +50,34 @@ public  class PlaceholderFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        for(int i=0;i<10;i++){
+            GifImageView gifImageView = new GifImageView(getActivity());
+            GifDrawable gd = null;
+            try {
+                gd = new GifDrawable(getActivity().getAssets(), "1.gif");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            gifImageView.setImageDrawable(gd);
+            list.add(gifImageView);
+        }
+
+
+        ViewPager viewPager = (ViewPager) rootView.findViewById(R.id.view_pager);
+        viewPager.setAdapter(new SimpleAdapter());
+    /*    GifImageView gifImageView = (GifImageView) rootView.findViewById(R.id.giv);
+        GifDrawable gd = null;
+        try {
+            gd = new GifDrawable(getActivity().getAssets(), "1.gif");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        gifImageView.setImageDrawable(gd);*/
+
         return rootView;
     }
+
 
     @Override
     public void onAttach(Activity activity) {
@@ -50,5 +85,33 @@ public  class PlaceholderFragment extends Fragment {
         //fragment attach到activity的时候就获取点击的item位置
         ((MainActivity) activity).onSectionAttached(
                 getArguments().getInt(ARG_SECTION_NUMBER));
+    }
+
+
+    private List<GifImageView> list = new ArrayList<>();
+
+
+    class SimpleAdapter extends PagerAdapter{
+
+        @Override
+        public int getCount() {
+            return 10;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            container.addView(list.get(position));
+            return list.get(position);
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+           container.removeView(list.get(position));
+        }
     }
 }
